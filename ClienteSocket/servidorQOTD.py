d923e7da4dcfb0576f519258d8652f5a0b89eb5c
Raquel <raqueelaller@hotmail.com>
2025-11-28 18:10:35 +0100

    Signed-off-by: Raquel <Hecho en clase>

--- ClienteSocket/servidorQOTD.py
+++ ClienteSocket/servidorQOTD.py
@@ -7,7 +7,7 @@ import random
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # enlazamos el socket a una dirección ip y a un puerto
-server_address= ('192.168.10.79', 2017)
+server_address= ('192.168.10.110', 2017)
 server_socket.bind(server_address)
 
 # ponemos el socket en modo escucha
@@ -16,7 +16,7 @@ print(f"servidor escuchando en {server_address}")
 
 frase: list[str] = ["vive y deja vivir", "Duerme menos, sueña más.","No dejes para mañana todo lo bonito que puedas hacer hoy :)", "Los mejores planes, no se planean"]
 
-numAleatorio:float = random.randint(0,len(frase))
+numAleatorio:int = random.randint(0,(len(frase)-1))
 
 while True:
 
@@ -24,14 +24,15 @@ while True:
     client_socket,client_address = server_socket.accept()
     print(f"conexión aceptada de {client_address}")
 # Manejar la conexión
-    response = frase.__getitem__(numAleatorio)
+    response = frase[numAleatorio]
     client_socket.sendall(response.encode('utf-8')) # Enviar respuesta
     # data = client_socket.recv(1024).decode('utf-8') #Recibir datos
     # print(f"Datos recibidos: {data}")
     
-    break
 # cerramos la conexión
     client_socket.close()
+
+
 
 
 
